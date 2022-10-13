d0517202ef2403ca9e46483e7908d20b7348ccc9
NF1825 <97575686+NF1825@users.noreply.github.com>
2022-10-13 14:44:51 -0400

    Initial Setup of Game
    Copied from the book this sets up the display window

--- game.py
+++ game.py
@@ -1,1 +1,27 @@
-print("Hello! Is there anybody out there?")
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class for game assets and behavior"""
+
+    def __init__(self):
+        """initialize the game"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1920,1080))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()

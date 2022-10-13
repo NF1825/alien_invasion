cbcda52b5351dcee40940cf67f60a05bf75ea24c
NF1825 <97575686+NF1825@users.noreply.github.com>
2022-10-13 15:16:11 -0400

    Set up the ship
    Set up the ship and fixed the ratio to fit into a nicer window and so that I can see the ship

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         #screen settings
-        self.screen_width = 1920
-        self.screen_height = 1080
+        self.screen_width = 1200
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)

c45f9103f6cd44b6f984b13520eb8ed32bcd320c
NF1825 <97575686+NF1825@users.noreply.github.com>
2022-10-20 14:54:29 -0400

    refactored create fleet

--- game.py
+++ game.py
@@ -93,9 +93,27 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """create the fleet of aliens."""
-        #make an alien.
+        #Create an alien and find the number of aliens in a row.
+        #spacing between each alien is equal to one alien width.
         alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        #create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number)
+
+
+
+    def _create_alien(self, alien_number):
+        """create an alien and place it in the row"""
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
         self.aliens.add(alien)
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""

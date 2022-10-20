7bc8e037d87bc4e44d5c0359d53d6e6fb531eb77
NF1825 <97575686+NF1825@users.noreply.github.com>
2022-10-20 14:34:26 -0400

    End of chapter 12

--- game.py
+++ game.py
@@ -28,8 +28,11 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+
+
+
 
 
     def _check_events(self):
@@ -69,8 +72,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """All in one update call for bullets"""
+        #update bullet positions.
+        self.bullets.update()
+
+        # get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 
     def _update_screen(self):

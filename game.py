edef8b4dfad9d9f7ca2376a1613473da0300effc
NF1825 <97575686+NF1825@users.noreply.github.com>
2022-10-20 15:09:48 -0400

    Aliens move right only

--- game.py
+++ game.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -89,6 +90,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
 
     def _create_fleet(self):
